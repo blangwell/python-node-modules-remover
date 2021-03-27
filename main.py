8bffa57ba3101bff7ec7ac05935b6f1d4d6ed389
blangwell <b.langwell@outlook.com>
2021-03-27 00:29:07 -0700

    Add send2trash functionality and user interactivity

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import os
+from colorama import Fore, Style
+from colorama import init as colorama_init
 from datetime import datetime
 from dateutil import relativedelta
+import math
+import os
+from send2trash import send2trash
+from termcolor import colored
 
 CWD = os.getcwd()
 ROOT_PATH = os.path.abspath(CWD)
 SUB_DIRS = [sub for sub in os.listdir(CWD) if os.path.isdir(sub)]
 TODAY = datetime.now()
+
+colorama_init()
+# print(colored('hello', 'green'))
 
 def find_old_projects(weeks):
     recents = []
@@ -15,25 +23,61 @@ def find_old_projects(weeks):
             recents.append(directory)
     return recents
 
-def get_nm_paths(directory):
+def get_nm_path(directory):
     results = []
     for dirpath, dirnames, filenames in os.walk(directory):
-        if 'node_modules' in dirnames:
-            results.append(os.path.join(dirpath, 'node_modules'))
-    print(ROOT_PATH + '/' + results[0] if len(results) > 0 else 'no node modules found')
+        if "node_modules" in dirnames:
+            results.append(os.path.join(dirpath, "node_modules"))
+    # print(ROOT_PATH + "/" + results[0] if len(results) > 0 else "no node modules found")
+    # if node modules found
     if len(results) > 0:
-        return ROOT_PATH + '/' + results[0]
+        # results[0] will be root node_modules dir
+        return ROOT_PATH + "/" + results[0]
 
-nm_paths = []
-old_projects = find_old_projects(12)
+def agg_nm_paths():
+    old_projects = find_old_projects(12)
+    nm_paths = []
+    # loop through old projects and populate nm_paths with nm paths
+    for project in old_projects:
+        nm = get_nm_path(project)
+        if nm: nm_paths.append(nm)
+    # print(nm_paths)
+    return nm_paths
 
-# loop through old projects and populate nm_paths with nm paths
-for project in old_projects:
-    nm = get_nm_paths(project)
-    if nm: nm_paths.append(nm)
+# loop through the nm_filepaths and send each directory to the trash
+def trash_nms(nm_paths):
+    for path in nm_paths:
+        print(f"### Sending {path} to trash ###")
+        send2trash(path)
 
-print(nm_paths)
+def main():
+    starting_prompt = """Welcome to the node_module spring cleaner.
+How many weeks of node modules do you want to keep? (Default 12)
+> """
+    weeks = input(starting_prompt)
+    try:
+        weeks = int(weeks)
+    except ValueError:
+        print(colored("Unrecognized input, defaulting to 12 weeks.", "red"))
+        weeks=12
+    print("Searching for old node_modules...")
+    old_projects = find_old_projects(weeks)
+    nm_paths = []
+    for project in old_projects:
+        nm = get_nm_path(project)
+        if nm: 
+            # print(f"Found {nm}")
+            nm_paths.append(nm)
+    print(f"\n##### Found " + colored(str(len(nm_paths)), 'blue', attrs=["bold"]) + " old node_modules directories! #####")
+    for nm in nm_paths:
+        print(nm)
+    send_to_trash = input(f"Send {len(nm_paths)} directories to the trash? y/n \n> ")
+    if send_to_trash.lower().strip() in ("y", "yes"):
+        # trash_nms(nm_paths) 
+        print("This is where trash_nms runs")
+        raise SystemExit("Files moved to trash")
+    else: 
+        raise SystemExit("Exiting...")
 
-# alt method to recursively get all subdirectories inside of old project
-# maybe_dirs = [x[0] for x in os.walk(old_projects[0])]
-# print(maybe_dirs)
+if __name__ == "__main__":
+    main()
