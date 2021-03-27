2a92e48af2f617d403cb4aaec1491575aa3ca45c
blangwell <b.langwell@outlook.com>
2021-03-26 20:15:57 -0700

    Add get_nm_paths function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dateutil import relativedelta
 
 CWD = os.getcwd()
 ROOT_PATH = os.path.abspath(CWD)
-SUB_DIRS = [name for name in os.listdir(CWD) if os.path.isdir(name)]
+SUB_DIRS = [sub for sub in os.listdir(CWD) if os.path.isdir(sub)]
 TODAY = datetime.now()
 
 def find_old_projects(weeks):
@@ -15,16 +15,25 @@ def find_old_projects(weeks):
             recents.append(directory)
     return recents
 
+def get_nm_paths(directory):
+    results = []
+    for dirpath, dirnames, filenames in os.walk(directory):
+        if 'node_modules' in dirnames:
+            results.append(os.path.join(dirpath, 'node_modules'))
+    print(ROOT_PATH + '/' + results[0] if len(results) > 0 else 'no node modules found')
+    if len(results) > 0:
+        return ROOT_PATH + '/' + results[0]
 
-print(find_old_projects(12))
+nm_paths = []
+old_projects = find_old_projects(12)
 
-# for directory in recents:
-    #     subdir_path = ROOT_PATH + '/' + directory
-    #     print([dirname for dirname in os.listdir(directory) if dirname == "node_modules"])
-    # for directory in recents:
-    #     subdir_path = ROOT_PATH + '/' + directory
-    #     f=[]
-    #     for dirpath, dirnames, filenames in os.walk(subdir_path):
-    #         f.extend(dirnames)
-    #         break
-    #     print(f, '📝')
+# loop through old projects and populate nm_paths with nm paths
+for project in old_projects:
+    nm = get_nm_paths(project)
+    if nm: nm_paths.append(nm)
+
+print(nm_paths)
+
+# alt method to recursively get all subdirectories inside of old project
+# maybe_dirs = [x[0] for x in os.walk(old_projects[0])]
+# print(maybe_dirs)
