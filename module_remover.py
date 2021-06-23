2fc47eb03ee32538a1eabc836aadbb08924fdbe2
blangwell <b.langwell@outlook.com>
2021-06-23 00:39:53 -0700

    Fix unrecognized weeks input bug

--- module_remover.py
+++ module_remover.py
@@ -17,7 +17,8 @@ ROOT_PATH = os.path.abspath(CWD)
 SUB_DIRS = [sub for sub in os.listdir(CWD) if os.path.isdir(sub)]
 TODAY = datetime.now()
 
-colorama_init() # windows term color compatibility
+colorama_init()  # windows term color compatibility
+
 
 def main():
     welcome()
@@ -26,6 +27,7 @@ def main():
     nm_paths = find_nm_dirs(old_projects)
     trash_yn(nm_paths)
 
+
 def find_old_projects(weeks):
     recents = []
     for directory in SUB_DIRS:
@@ -33,6 +35,7 @@ def find_old_projects(weeks):
         if dir_date_time < TODAY - relativedelta.relativedelta(weeks=weeks):
             recents.append(directory)
     return recents
+
 
 def get_nm_path(directory):
     results = []
@@ -43,11 +46,13 @@ def get_nm_path(directory):
         # results[0] will be root node_modules dir
         return ROOT_PATH + "/" + results[0]
 
+
 def find_nm_dirs(dirs):
     loading = True
 
     def spinner():
-        spinner = Spinner(colored("Searching for old node module dirs ", "cyan"))
+        spinner = Spinner(
+            colored("Searching for old node module dirs ", "cyan"))
         while loading:
             time.sleep(.2)
             spinner.next()
@@ -57,7 +62,7 @@ def find_nm_dirs(dirs):
     nm_paths = []
     for folder in dirs:
         nm = get_nm_path(folder)
-        if nm: 
+        if nm:
             nm_paths.append(nm)
 
     loading = False
@@ -68,40 +73,48 @@ def find_nm_dirs(dirs):
 
     hash_hr = colored("########", "blue")
     count = colored(str(len(nm_paths)), 'cyan', attrs=["bold"])
-    found = colored(" Found ", "green") + count + colored(" old node_modules directories ", "green")
+    found = colored(" Found ", "green") + count + \
+        colored(" old node_modules directories ", "green")
     print("\n" + hash_hr + found + hash_hr)
     for nm in nm_paths:
         print(nm)
     return nm_paths
 
+
 def get_weeks():
-    weeks = input("How many weeks of node modules do you want to keep? (Default 12)\n") + "> "
+    weeks = input("How many weeks of node modules do you want to keep? (Default 12)\n> ")
     try:
         weeks = int(weeks)
+        return weeks
     except ValueError:
         cprint("Unrecognized input, defaulting to 12 weeks.", "red", file=sys.stderr)
-        weeks=12
-    return weeks
+        return 12
 
 # loop through the nm_filepaths and send each directory to the trash
+
+
 def trash_nms(nm_paths):
     for path in nm_paths:
         print(colored(f"Trashing {path}", "cyan"))
         send2trash(path)
 
+
 def trash_yn(dirs):
-    response = input(colored(f"Send {len(dirs)} directories to the trash? y/n \n", "yellow") + "> ")
+    response = input(
+        colored(f"Send {len(dirs)} directories to the trash? y/n \n", "yellow") + "> ")
     if response.lower().strip() in ("y", "yes"):
-        trash_nms(dirs) 
-        raise SystemExit(colored(f"{len(dirs)} directories successfully moved to trash\n", "green") + "Exiting...")
-    else: 
-        raise SystemExit(colored("No directories moved to trash. \n", "cyan") + "Exiting...")
+        trash_nms(dirs)
+        raise SystemExit(colored(
+            f"{len(dirs)} directories successfully moved to trash\n", "green") + "Exiting...")
+    else:
+        raise SystemExit(
+            colored("No directories moved to trash. \n", "cyan") + "Exiting...")
+
 
 def welcome():
-    hash_hr  = "\n######################################################\n"
+    hash_hr = "\n######################################################\n"
     welcome = "\n Welcome to the node_module remover tool v0.2 \n"
     print(hash_hr + welcome + hash_hr)
-
 
 
 if __name__ == "__main__":
